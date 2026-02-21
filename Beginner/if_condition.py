e6526ffa1ee6cfa71c022da1dc35954657ae3b9d
Md Abuobaida <abuobaidamd036@gmail.com>
2026-02-21 16:47:00 +0530

    Create if_condition.py

---
+++ Beginner/if_condition.py
@@ -0,0 +1,13 @@
+height = float(input("Enter height: "))
+weight = float(input("Enter weight: "))
+
+bmi = weight / (height ** 2)
+
+if bmi >= 30:
+    print("Obesity")
+elif 25 <= bmi < 30:
+    print("Overweight")
+elif 18.5 <= bmi < 25:
+    print("Normal")
+else:
+    print("Underweight")
