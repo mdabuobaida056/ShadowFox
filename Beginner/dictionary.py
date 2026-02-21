5c50229cd3808051cdb59450ce601688d8708fbf
Md Abuobaida <abuobaidamd036@gmail.com>
2026-02-21 16:49:31 +0530

    Create dictionary.py

---
+++ Beginner/dictionary.py
@@ -0,0 +1,14 @@
+your_expenses = {
+    "Hotel": 1200,
+    "Food": 800,
+    "Travel": 500
+}
+
+partner_expenses = {
+    "Hotel": 1000,
+    "Food": 900,
+    "Travel": 600
+}
+
+print("Your Total:", sum(your_expenses.values()))
+print("Partner Total:", sum(partner_expenses.values()))
